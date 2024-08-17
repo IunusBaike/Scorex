21948d0c9e249fd9c0a5d1358bbbb305b71ec973
Aruuke <kutbidin_kyzy_a@iuca.kg>
2024-08-17 15:54:30 +0600

    added box

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import girl from "../assets/girl.svg"
 
 function About() {
   return (
     <div>
-      About
+   
+    <h1>what our clients say</h1>
+    <p>The complete suite of enterprise management applications for Ad - serving, Push messaging and Hood Analyticsintegrated with the largest advertising networks in the industry.</p>
+    <h3>We've been in the ad tech industry for over 7 years and we never had such a comprehensive platform like Scorex</h3>
+     <img src={girl} alt="" />
     </div>
   )
 }
