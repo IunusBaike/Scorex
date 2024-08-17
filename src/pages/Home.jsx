31d1e8d3ea771f617003a18e8b5a4cf6f213028e
Iunus <iussko1@gmail.com>
2024-08-17 12:49:32 +0300

    added scorex-2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import grup from "../assets/grup.png"
+import group from "../assets/group.png"
+import group2 from "../assets/group2.png"
 
 function Home() {
   return (
@@ -31,6 +33,16 @@ function Home() {
           <p>Integrate our service with JIRA and Gitlab to effortlessly track task statuses, trace changes, and import projects. Simply add your API key, and witness a harmonious collaboration between our platform and your existing workflow tools.</p>
         </div>
 
+    <div className="front">
+      <div>
+        <img src={group2} alt="" />
+        <h1>Revolutionize the way you manage your workforce with Scorex </h1>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </div>
+    </div>
 
       </div>
     </>
